faf4e357037061b2191b50846d89ba8e34e1c65a
AlexUG0 <aleksejugolkov177@gmail.com>
2025-03-11 21:54:23 +0400

    moduls done

--- src/category.py
+++ src/category.py
@@ -1,14 +1,29 @@
+from src.product import Product
+
+
 class Category:
     """ Класс для описания категорий товаров """
     name: str
     description: str
     products: list
+
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
-        Category.category_count += 1
-        Category.product_count += len(products)
+        self.__products = products if products else []
+        # Category.category_count += 1
+        Category.product_count += len(products) if products else 0
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
